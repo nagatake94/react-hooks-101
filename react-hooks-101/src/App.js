901c842888f4a07bb8b8e8935aeebff579adc7ab
nagatak <76759306+nagatake94@users.noreply.github.com>
2021-03-21 09:07:43 +0900

    implement component using multiple status

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,33 +1,28 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
+const App = props => {
+   
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
+  }
  
-  const increment2 = () => setCount( previousCount => previousCount + 1 );
-  const decrement2 = () => setCount( previousCount => previousCount - 1 );
-
-  const reset = () => setCount(0);
-  const double = () => setCount( count*2 );
-
   return (
     <React.Fragment>
-       <div>count:{count}</div>
-       <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-       </div>
-       <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-       </div>
-       <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={double}>×2</button>
-       </div>
-    </React.Fragment>
-  );
+      <p>現在の{name}は{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name}　onChange = {e => setName(e.target.value)} />
+    </React.Fragment>);
+};
+
+App.defaultProps = {
+  name : "サンプル",
+  price: 1000
 }
 
 export default App;
