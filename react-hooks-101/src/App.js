187245deccabec843e19297b45470b7b359c6b9a
nagatak <76759306+nagatake94@users.noreply.github.com>
2021-03-21 10:31:01 +0900

    implement object state

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
 
 const App = props => {
-   
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
+  
+  const [state, setState] = useState(props);
+  const {name, price} = state;
 
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  }
- 
   return (
     <React.Fragment>
-      <p>現在の{name}は{price}円です。</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name}　onChange = {e => setName(e.target.value)} />
+      <p>現在の{state.name}は{price}円です。</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={state.name}　onChange = {e => setState({...state, name: e.target.value})} />
     </React.Fragment>);
 };
 
 App.defaultProps = {
-  name : "サンプル",
+  name : "",
   price: 1000
 }
 
